ffe8130394ff9084422d1c58d8f86f10fe6d854b
anth <antlsr396@gmail.com>
2025-04-09 02:48:42 +0800

    Added items

--- app/src/main/java/com/example/groceries/GroceryData.java
+++ app/src/main/java/com/example/groceries/GroceryData.java
@@ -8,19 +8,66 @@ public class GroceryData {
         put("Alcohol", new HashMap<String, Integer>(){{
             put("Beer", R.drawable.product_beer);
             put("Champagne", R.drawable.product_champagne);
-            put("Wine", R.drawable.product_wine);
+            put("Red Wine", R.drawable.product_red_wine);
+            put("White Wine", R.drawable.product_white_wine);
+            put("Whiskey", R.drawable.product_whiskey);
+            put("Soju", R.drawable.product_soju);
+            put("Vodka", R.drawable.product_vodka);
+            put("Tequila", R.drawable.product_tequila);
+            put("Brandy", R.drawable.product_brandy);
+            put("Gin", R.drawable.product_gin);
+            put("Rum", R.drawable.product_rum);
+            put("Cider", R.drawable.product_cider);
+            put("Absinthe", R.drawable.product_absinthe);
+            put("Sake", R.drawable.product_sake);
+            put("Baijiu", R.drawable.product_baijiu);
         }});
         put("Bakery", new HashMap<String, Integer>(){{
             put("Bread", R.drawable.product_bread);
             put("Muffin", R.drawable.product_muffin);
+            put("Croissant", R.drawable.product_croissant);
+            put("Baguette", R.drawable.product_baguette);
+            put("Doughnut", R.drawable.product_doughnut);
+            put("Bagel", R.drawable.product_bagel);
+            put("Cake", R.drawable.product_cake);
+            put("Pita", R.drawable.product_pita);
+            put("Scone", R.drawable.product_scone);
+            put("Cupcake", R.drawable.product_cupcake);
+            put("Biscuit", R.drawable.product_biscuit);
+            put("Tortilla", R.drawable.product_tortilla);
         }});
         put("Beauty", new HashMap<String, Integer>(){{
             put("Lip Stick", R.drawable.product_lipstick);
             put("Mascara", R.drawable.product_mascara);
+            put("Foundation", R.drawable.product_foundation);
+            put("Eyeliner", R.drawable.product_eyeliner);
+            put("Concealer", R.drawable.product_concealer);
+            put("Blush", R.drawable.product_blush);
+            put("Highlighter", R.drawable.product_highlighter);
+            put("Compact Powder", R.drawable.product_compact_powder);
+            put("Makeup Remover", R.drawable.product_makeup_remover);
+            put("Nail Polish", R.drawable.product_nail_polish);
+            put("Face Cream", R.drawable.product_face_cream);
+            put("Face Mask", R.drawable.product_face_mask);
+            put("Toner", R.drawable.product_toner);
+            put("Serum", R.drawable.product_serum);
+            put("Lip Balm", R.drawable.product_lip_balm);
         }});
         put("Beverages", new HashMap<String, Integer>(){{
-            put("Cola", R.drawable.product_cola);
+            put("Soft Drink", R.drawable.product_soft_drink);
             put("Water", R.drawable.product_water);
+            put("Juice", R.drawable.product_juice);
+            put("Iced Tea", R.drawable.product_iced_tea);
+            put("Lemonade", R.drawable.product_lemonade);
+            put("Energy Drink", R.drawable.product_energy_drink);
+            put("Coconut Water", R.drawable.product_coconut_water);
+            put("Coffee", R.drawable.product_coffee);
+            put("Milkshake", R.drawable.product_milkshake);
+            put("Smoothie", R.drawable.product_smoothie);
+            put("Sparkling Water", R.drawable.product_sparkling_water);
+            put("Sports Drink", R.drawable.product_sports_drink);
+            put("Hot Chocolate", R.drawable.product_hot_chocolate);
+            put("Herbal Tea", R.drawable.product_herbal_tea);
         }});
         put("Candy", new HashMap<String, Integer>(){{
             put("Apple", R.drawable.product_apple);
@@ -51,6 +98,7 @@ public class GroceryData {
             put("Banana", R.drawable.product_banana);
             put("Orange", R.drawable.product_orange);
             put("Grape", R.drawable.product_grape);
+            put("Pear", R.drawable.product_pear);
         }});
         put("Grain", new HashMap<String, Integer>(){{
             put("Pasta", R.drawable.product_pasta);
